74efa6d553ace6f288d8bda12cf7fe03803b76a7
KazariEX <1364035137@qq.com>
2024-09-30 23:49:11 +0800

    更新依赖

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,50 +1,50 @@
 import { defineValaxyConfig } from "valaxy";
-import type { UserThemeConfig } from "valaxy-theme-yun";
 import { addonLightGallery } from "valaxy-addon-lightgallery";
 import { addonPermalink } from "valaxy-addon-permalink";
+import type { UserThemeConfig } from "valaxy-theme-yun";
 
 const safelist = [
-  "i-ri-home-line"
+    "i-ri-home-line"
 ];
 
 export default defineValaxyConfig<UserThemeConfig>({
-  theme: "yun",
-  themeConfig: {
-    banner: {
-      enable: true,
-      title: "蜡羽集",
-      cloud: {
-        enable: true
-      }
+    theme: "yun",
+    themeConfig: {
+        banner: {
+            enable: true,
+            title: "蜡羽集",
+            cloud: {
+                enable: true
+            }
+        },
+        pages: [
+            {
+                name: "图集",
+                url: "/gallery/",
+                icon: "i-ri-folder-image-line",
+                color: "hotpink"
+            },
+            {
+                name: "我的小伙伴们",
+                url: "/links/",
+                icon: "i-ri-genderless-line",
+                color: "dodgerblue"
+            }
+        ],
+        footer: {
+            since: 2023,
+            beian: {
+                enable: false,
+                icp: ""
+            }
+        },
+        colors: {
+            primary: "#beb4b3"
+        }
     },
-    pages: [
-      {
-        name: "图集",
-        url: "/gallery/",
-        icon: "i-ri-folder-image-line",
-        color: "hotpink"
-      },
-      {
-        name: "我的小伙伴们",
-        url: "/links/",
-        icon: "i-ri-genderless-line",
-        color: "dodgerblue"
-      }
+    addons: [
+        addonLightGallery(),
+        addonPermalink()
     ],
-    footer: {
-      since: 2023,
-      beian: {
-        enable: false,
-        icp: ""
-      }
-    },
-    colors: {
-        primary: "#beb4b3"
-    }
-  },
-  addons: [
-    addonLightGallery(),
-    addonPermalink()
-  ],
-  unocss: { safelist }
+    unocss: { safelist }
 });
